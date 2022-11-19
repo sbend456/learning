227fc245638bcd41641b5839122e6c97f2ba8c32
sbend456 <sbend456@gmail.com>
2022-11-20 01:09:17 +0200

    increment

--- dispatcher.c
+++ dispatcher.c
@@ -4,7 +4,7 @@
 #include "mail.h"
 
 // extern process_t proc[4];
-char dev_in[4][20]={"/dev/pts/2" , "/dev/pts/4","/dev/pts/6", "/dev/pts/8"};
+char dev_in[4][20]={"/dev/pts/7" , "/dev/pts/9","/dev/pts/11", "/dev/pts/13"};
 
 int dispacher_fuc(process_info *p){
     DEBUG(("dispatch started"));
@@ -13,12 +13,13 @@ int dispacher_fuc(process_info *p){
     {
         //recevoir evenemt
         OBJECT *obj_ptr=receive(p->out);
-        DEBUG(("Dispatcher RCV DEPART : %d",obj_ptr->event.department));
+        DEBUG(("Dispatcher RCV DEPART : %d, %s",\
+        obj_ptr->event.department,dev_in[obj_ptr->event.department]));
         // DEBUG(("departement:") );
         // DEBUG(("departement: %d",obj_ptr->event.department) );
         // DEBUG(("sending to : %s",.));
         //verifier departemt de event
-        // send(proc[0].info.in,obj_ptr);
+        send(dev_in[obj_ptr->event.department],obj_ptr);
         //envoyer au bn process
         sleep(1);
         DEBUG(("dispacher %d", count++));
